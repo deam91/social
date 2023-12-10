4095d3048b522af3edf029de16963100c576d633
Damian Diaz <deamdiaz27@gmail.com>
2023-12-10 14:52:58 -0800

    Add basic helmet use

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -23,6 +24,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(helmet());
   await app.listen(3000);
   console.log('Server started at port 3000...');
 }
