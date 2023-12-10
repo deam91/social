f44cb34a62a401cabe2ba9abf672f65b3577a1e9
Damian Diaz <deamdiaz27@gmail.com>
2023-12-10 14:49:25 -0800

    Handle authentication  - Add login and profile endpoints  - Add jwt basic authentication   - Add guards definitions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import * as process from 'process';
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env.development',
