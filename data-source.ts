9fda719ebddfeab8815617371f3cb564f52732b9
Damian Diaz <deamdiaz27@gmail.com>
2023-12-10 14:42:29 -0800

    Add typeorm datasource definition

--- data-source.ts
+++ data-source.ts
@@ -1,22 +1,22 @@
-import {DataSource} from 'typeorm';
+import { DataSource } from 'typeorm';
 import * as path from 'path';
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "social",
-    synchronize: true, // set to false in production
-    logging: true,
-    entities: [
-        path.join(__dirname, '/**/*.entity{.ts,.js}') // Path to your entity files
-    ],
-    migrations: [
-        path.join(__dirname, './dist/migration/**/*.ts') // Path to your migration files
-    ],
-    subscribers: [],
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'password',
+  database: 'social',
+  synchronize: true, // set to false in production
+  logging: true,
+  entities: [
+    path.join(__dirname, '/**/*.entity{.ts,.js}'), // Path to your entity files
+  ],
+  migrations: [
+    path.join(__dirname, './dist/migration/**/*.ts'), // Path to your migration files
+  ],
+  subscribers: [],
 });
 
 // AppDataSource.initialize()
