85296c3d0bdce14feeae579232a58c6e7ef044de
Damian Diaz <deamdiaz27@gmail.com>
2024-01-10 21:15:11 -0800

    fix: use retry from rxjs

--- social/src/core/interceptors/retry.interceptor.ts
+++ social/src/core/interceptors/retry.interceptor.ts
@@ -4,22 +4,16 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retryWhen, delay, take } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, delay, retry, delayWhen } from 'rxjs/operators';
 
 @Injectable()
 export class RetryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      retryWhen((errors) =>
-        errors.pipe(
-          delay(1000), // delay between retries
-          take(3), // maximum number of retries
-          catchError((err) => {
-            return throwError(err);
-          }), // throw error if maximum retries exceeded
-        ),
-      ),
+      retry(3), // maximum number of retries
+      delayWhen(() => of(true).pipe(delay(1000))), // delay between retries
+      catchError((err) => throwError(err)), // throw error if maximum retries exceeded
     );
   }
 }
